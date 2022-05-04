cacb445a0ba557ac0381fa6adf51a83fc640c106
Mr.GeschVed <86187817+chilledboyzrblx@users.noreply.github.com>
2022-05-04 13:59:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 song1 = "";
 song2 = "";
+
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
-function preload(){
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
+song1_status = "";
+song2_status = "";
+
+function preload() {
     song1 = loadSound('music1.mp3');
     song2 = loadSound('music2.mp3');
 }
 
-function setup(){
-    canvas = createCanvas(600,500);
+function setup() {
+    canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
@@ -20,19 +27,43 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
+function modelLoaded() {
     console.log('PoseNet has started successfully');
 }
 
-function draw(){
-    image(video,0,0,600,500);
+function draw() {
+    image(video, 0, 0, 600, 500);
+    fill("#0044ff");
+    stroke("#0044ff");
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+        if (song1_status == false) {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Song Name: Alan Walker Type Beat 2";
+        }
+    }
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if (song2_status == false) {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Song Name: Heaven";
+        }
+    }
 }
 
-function gotPoses(results){
-    if(results.length > 0){
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log('leftWristX = '+leftWristX+', leftWristY = '+leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log('leftWristX = ' + leftWristX + ', leftWristY = ' + leftWristY);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
     }
 }
